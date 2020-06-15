441f671481f41a03fb863dcdaca89c960cca6465
annakrystalli <annakrystalli@googlemail.com>
2020-06-15 15:31:00 +0100

    add tibble of available services

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' EMODnet Web Feature Services
+#'
+#' @format A tibble of available EMODnet Web Feature Services
+#' \describe{
+#'   \item{service_name}{Service name}
+#'   \item{service_url}{Service URL}
+#'   }
+"emodnet_wfs"
