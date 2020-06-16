79ae638ab6aa8313c82370a6f7016da240bd3647
annakrystalli <annakrystalli@googlemail.com>
2020-06-16 12:14:03 +0100

    add get_info test

--- tests/testthat/test-info.R
+++ tests/testthat/test-info.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("wfs info works for all services", {
+    l_info <- purrr::map(emodnet_wfs$service_name,
+                         ~try(emodnet_get_wfs_info(service = .x)))
+
+    info_classes <- purrr::map(l_info, class) %>% unique() %>% unlist()
+    expect_setequal(info_classes, c("tbl_df", "tbl", "data.frame"))
 })
