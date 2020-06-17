e124eb952de85b052fe2a01614a83db1edabeb9c
annakrystalli <annakrystalli@googlemail.com>
2020-06-17 10:31:14 +0100

    add test for get_info from wfs object

--- tests/testthat/test-info.R
+++ tests/testthat/test-info.R
@@ -1,4 +1,4 @@
-test_that("wfs info works for all services", {
+test_that("wfs info works for all services from server", {
     l_info <- purrr::map(emodnet_wfs$service_name,
                          ~try(emodnet_get_wfs_info(service = .x)))
 
@@ -6,9 +6,17 @@ test_that("wfs info works for all services", {
     expect_setequal(info_classes, c("tbl_df", "tbl", "data.frame"))
 })
 
+wfs_cml <- emodnet_init_wfs_client("chemistry_marine_litter")
+
+test_that("wfs info works on wfs object", {
+    layer_info_all <- emodnet_get_wfs_info(wfs_cml)
+    expect_s3_class(layer_info_all,
+                    class = c("tbl_df", "tbl", "data.frame"))
+    expect_gt(nrow(layer_info_all), 0)
+})
+
 
 test_that("emodnet_get_layer_info works", {
-    wfs_cml <- emodnet_init_wfs_client("chemistry_marine_litter")
     layers <- c("bl_fishing_cleaning",
                "bl_beacheslocations_2001_2008_monitoring")
     layer_info_cml <-emodnet_get_layer_info(
