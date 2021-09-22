f120d6d9f5ba4382be4ed7debd6d048ba7b44a0a
Anna Krystalli <annakrystalli@googlemail.com>
2021-09-22 17:06:50 +0100

    add categorical & numeric filtering tests

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -1,3 +1,38 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("categorical filters work", {
+    wfs <- emodnet_init_wfs_client(service = "geology_seabed_substrate_maps")
+  expect_equal(emodnet_get_layers(wfs = wfs,
+                                  layers = "seabed_substrate_1m",
+                                  cql_filter = "country='Baltic Sea'",
+                                  reduce_layers = TRUE)$country %>% unique(), 'Baltic Sea')
+
+
+   or_filter_sf <- emodnet_get_layers(wfs = wfs, layers = "seabed_substrate_1m",
+                       cql_filter = "country='Baltic Sea' OR country='Bulgaria'",
+                       reduce_layers = TRUE )
+
+   expect_equal(or_filter_sf$country %>% unique(), c("Bulgaria", "Baltic Sea"))
+
 })
+
+
+test_that("numeric filters work", {
+
+    num_filter_sf <- emodnet_get_layers(wfs = wfs, layers = "seabed_substrate_1m",
+                                       cql_filter = "country='Bulgaria' AND shape_length>1",
+                                       reduce_layers = TRUE )
+
+    expect_equal(num_filter_sf$country %>% unique(), c("Bulgaria"))
+    expect_true(min(num_filter_sf$shape_length) > 1)
+
+
+    num_filter_sf <- emodnet_get_layers(wfs = wfs, layers = "seabed_substrate_1m",
+                       cql_filter = "(country='Baltic Sea' OR country='Bulgaria') AND shape_length<1",
+                       reduce_layers = TRUE )
+
+    expect_equal(num_filter_sf$country %>% unique(), c("Bulgaria", "Baltic Sea"))
+    expect_true(min(num_filter_sf$shape_length) < 1)
+
+})
+
+
+
