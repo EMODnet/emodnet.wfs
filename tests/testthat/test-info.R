bd82d74f0fcec7ced3481ef070e1c3333c57519c
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-03-02 15:46:36 +0100

    httptest here too

--- tests/testthat/test-info.R
+++ tests/testthat/test-info.R
@@ -1,11 +1,10 @@
 test_that("wfs info works from the server for a random service", {
     skip_on_os("linux")
+    skip_on_cran()
     service_name <- sample(emodnet_wfs$service_name, 1)
-    info <- emodnet_get_wfs_info(
-        service = service_name)
+    info <- emodnet_get_wfs_info(service = service_name)
 
-    expect_s3_class(info,
-                    class = c("tbl_df", "tbl", "data.frame"))
+    expect_s3_class(info, class = c("tbl_df", "tbl", "data.frame"))
     expect_gt(nrow(info), 0)
     expect_setequal(unique(info$service_name), service_name)
 })
@@ -13,37 +12,40 @@ test_that("wfs info works from the server for a random service", {
 test_that("wfs all info works", {
     skip_on_os("linux")
     skip_on_ci()
+    skip_on_cran()
     all_info <- emodnet_get_all_wfs_info()
-    expect_s3_class(all_info,
-                    class = c("tbl_df", "tbl", "data.frame"))
+    expect_s3_class(all_info, class = c("tbl_df", "tbl", "data.frame"))
     expect_gt(nrow(all_info), 0)
     expect_setequal(unique(all_info$service_name), emodnet_wfs$service_name)
 })
 
 
 test_that("wfs info works on wfs object", {
-    wfs <- emodnet_init_wfs_client("biology")
-    layer_info_all <- emodnet_get_wfs_info(wfs)
-    expect_s3_class(layer_info_all,
-                    class = c("tbl_df", "tbl", "data.frame"))
+    with_mock_dir("biology-info", {
+        wfs <- emodnet_init_wfs_client("biology")
+        layer_info_all <- emodnet_get_wfs_info(wfs)
+    })
+
+    expect_s3_class(layer_info_all, class = c("tbl_df", "tbl", "data.frame"))
     expect_gt(nrow(layer_info_all), 0)
-    expect_equal(unique(layer_info_all$service_name),  "biology")
-    expect_equal(unique(layer_info_all$service_url),
-                 "http://geo.vliz.be/geoserver/Emodnetbio/wfs")
+    expect_equal(unique(layer_info_all$service_name), "biology")
+    expect_equal(unique(layer_info_all$service_url), "http://geo.vliz.be/geoserver/Emodnetbio/wfs")
 })
 
 
 test_that("emodnet_get_layer_info works", {
-    wfs <- emodnet_init_wfs_client("biology")
-    layers <- c("mediseh_zostera_m_pnt", "mediseh_cymodocea_pnt")
-    layer_info_cml <-emodnet_get_layer_info(
-        wfs = wfs,
-        layers = layers
-    )
+    with_mock_dir("biology-info", {
+        wfs <- emodnet_init_wfs_client("biology")
+        layers <- c("mediseh_zostera_m_pnt", "mediseh_cymodocea_pnt")
+        layer_info_cml <- emodnet_get_layer_info(
+            wfs = wfs,
+            layers = layers
+        )
+    })
     expect_equal(nrow(layer_info_cml), 2)
     expect_setequal(layer_info_cml$layer_name, layers)
     expect_s3_class(layer_info_cml,
-                    class = c("tbl_df", "tbl", "data.frame"))
+        class = c("tbl_df", "tbl", "data.frame"))
 })
 
 
