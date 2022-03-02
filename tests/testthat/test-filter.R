15a671728b271cb491d1ee6de8b0d73723bf3cc8
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-03-02 09:39:18 +0100

    fix or new error?

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -1,40 +1,43 @@
-wfs <- emodnet_init_wfs_client(service = "geology_seabed_substrate_maps")
 
 test_that("categorical filters work", {
+    wfs <- emodnet_init_wfs_client(service = "chemistry_marine_litter")
 
-  expect_equal(emodnet_get_layers(wfs = wfs,
-                                  layers = "seabed_substrate_1m",
-                                  cql_filter = "country='Baltic Sea'",
-                                  reduce_layers = TRUE)$country %>% unique(), 'Baltic Sea')
+    simple_filter_sf <- emodnet_get_layers(
+        wfs = wfs,
+        layers = "sl_fishing",
+        cql_filter = "country='Baltic Sea'",
+        reduce_layers = TRUE
+    )
+    expect_equal(unique(simple_filter_sf$country), 'Baltic Sea')
 
-
-   or_filter_sf <- emodnet_get_layers(wfs = wfs, layers = "seabed_substrate_1m",
-                       cql_filter = "country='Baltic Sea' OR country='Bulgaria'",
-                       reduce_layers = TRUE )
-
-   expect_equal(or_filter_sf$country %>% unique(), c("Bulgaria", "Baltic Sea"))
-
+    or_filter_sf <- emodnet_get_layers(
+        wfs = wfs,
+        layers = "sl_fishing",
+        cql_filter = "country='Baltic Sea' OR country='Bulgaria'",
+        reduce_layers = TRUE
+    )
+    expect_equal(unique(or_filter_sf$country), c("Bulgaria", "Baltic Sea"))
 })
 
 
 test_that("numeric filters work", {
+    wfs <- emodnet_init_wfs_client(service = "chemistry_marine_litter")
 
-    num_filter_sf <- emodnet_get_layers(wfs = wfs, layers = "seabed_substrate_1m",
-                                       cql_filter = "country='Bulgaria' AND shape_length>1",
-                                       reduce_layers = TRUE )
-
-    expect_equal(num_filter_sf$country %>% unique(), c("Bulgaria"))
+    num_filter_sf <- emodnet_get_layers(
+        wfs = wfs, layers = "sl_fishing",
+        cql_filter = "country='Bulgaria' AND shape_length>1",
+        reduce_layers = TRUE
+    )
+    expect_equal(unique(num_filter_sf$country), c("Bulgaria"))
     expect_true(min(num_filter_sf$shape_length) > 1)
 
 
-    num_filter_sf <- emodnet_get_layers(wfs = wfs, layers = "seabed_substrate_1m",
-                       cql_filter = "(country='Baltic Sea' OR country='Bulgaria') AND shape_length<1",
-                       reduce_layers = TRUE )
-
-    expect_equal(num_filter_sf$country %>% unique(), c("Bulgaria", "Baltic Sea"))
+    num_filter_sf <- emodnet_get_layers(
+        wfs = wfs,
+        layers = "sl_fishing",
+        cql_filter = "(country='Baltic Sea' OR country='Bulgaria') AND shape_length<1",
+        reduce_layers = TRUE
+    )
+    expect_equal(unique(num_filter_sf$country), c("Bulgaria", "Baltic Sea"))
     expect_true(min(num_filter_sf$shape_length) < 1)
-
 })
-
-
-
