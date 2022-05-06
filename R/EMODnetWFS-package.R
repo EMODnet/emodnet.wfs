c9e2c4ca62ce97bf0c4b52f686f30318bda81b20
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-05-06 16:11:55 +0200

    finer user-agent

--- R/EMODnetWFS-package.R
+++ R/EMODnetWFS-package.R
@@ -7,5 +7,12 @@
 NULL
 
 emodnetwfs_user_agent <- function() {
+  if (whoami::gh_username() %in% c("annakrystalli", "salvafern", "maelle")) {
+  	return("EMODnetWFS R package DEV https://github.com/EMODnet/EMODnetWFS")
+  }
+  if (nzchar(Sys.getenv("EMODNETWFS_CI"))) {
+  	return("EMODnetWFS R package CI https://github.com/EMODnet/EMODnetWFS")
+  }
+
   "EMODnetWFS R package https://github.com/EMODnet/EMODnetWFS"
 }
