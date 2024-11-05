9ac33073affc01ddc9e1dad349a90a29f905a3c0
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-11-05 15:35:55 +0100

    docs: description and safer example for `emodnet_wfs()`

--- R/emodnet_wfs.R
+++ R/emodnet_wfs.R
@@ -6,9 +6,11 @@
 }
 #' Available EMODnet Web Feature Services
 #'
+#' Which data sources (services) are available?
+#'
 #' @return Tibble of available EMODnet Web Feature Services
 #'
-#' @examples
+#' @examplesIf interactive()
 #' emodnet_wfs()
 #' @export
 emodnet_wfs <- memoise::memoise(.emodnet_wfs)
